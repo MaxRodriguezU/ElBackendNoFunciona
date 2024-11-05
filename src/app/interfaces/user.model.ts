c5984593919a51111c9af7945137cce461d7aa9e
Maximiliano <max.rodriguezu@duocuc.cl>
2024-11-05 14:41:33 -0300

    userModel

---
+++ src/app/interfaces/user.model.ts
@@ -0,0 +1,12 @@
+// src/app/models/user.model.ts
+
+export interface User {
+    id: string;
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    gender: 'male' | 'female';
+    accessToken: string;
+  }
+  
