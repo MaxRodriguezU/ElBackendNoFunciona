dff5154efe82cbd2db632fb7d340c724ae1a7b69
Maximiliano <max.rodriguezu@duocuc.cl>
2024-11-05 12:36:06 -0300

    ProductPageReady

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  products: any[] = [];
+  skip: number = 0;
+  total: number = 0;
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.loadProducts();
+  }
+
+  loadProducts(event?: any) {
+    this.productService.getProducts(this.skip).subscribe(
+      (response) => {
+        this.products = [...this.products, ...response.products];
+        this.total = response.total;
+        this.skip += response.limit;
+
+        if (event) {
+          event.target.complete();
+        }
+
+        // Si se han cargado todos los productos, desactivar infinite scroll
+        if (this.products.length >= this.total && event && event.target) {
+          event.target.disabled = true;
+        }
+      },
+      (error) => {
+        console.error('Error al cargar productos', error);
+        if (event) {
+          event.target.complete();
+        }
+      }
+    );
+  }
+
+  loadMore(event: any) {
+    this.loadProducts(event);
   }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private apiUrl = 'https://dummyjson.com/auth/products';
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(skip: number = 0): Observable<any> {
+    const token = localStorage.getItem('accessToken');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.get(`${this.apiUrl}?skip=${skip}`, { headers });
+  }
 }
