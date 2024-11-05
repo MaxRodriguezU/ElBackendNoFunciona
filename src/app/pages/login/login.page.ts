baa145ee2b983a1f875f2bd1b924fa20caa11584
Maximiliano <max.rodriguezu@duocuc.cl>
2024-11-05 12:21:56 -0300

    LoginPageReady

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  username: string = '';
+  password: string = '';
+  loginError: string | null = null;
+
+  constructor(
+    private authService: AuthService, 
+    private navCtrl: NavController
+  ) { }
 
   ngOnInit() {
+  }
+
+  onLogin() {
+    this.authService.login(this.username, this.password).subscribe(
+      (response) => {
+        // Guardar el token en localStorage
+        this.authService.saveToken(response.accessToken);
+        // Navegar a la página principal
+        this.navCtrl.navigateRoot('/home');
+      },
+      (error) => {
+        // Mostrar mensaje de error en caso de fallo
+        this.loginError = 'Usuario o contraseña incorrectos.';
+      }
+    );
   }
 
 }
